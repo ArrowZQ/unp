9bfdb1b60d725233c8e0058e116560a4af2cc444
Chen Fangzhou <chenfzh@gmail.com>
2012-04-26 11:57:37 +0800

    new file:   tcpcliserv/tcpcli 	new file:   udpcliserv/udpcli 	new file:   udpcliserv/udpserv 	new file:   udpcliserv/udpservselect.c     使用了 select 的服务端，使用了tcp 和 udp，客户端tcpcli，udpcli都可以连接过来, tcpcli也使用了select(监控标准输入和socket，这两个描述符)

---
+++ unp_cfz/udpcliserv/udpservselect.c
@@ -0,0 +1,78 @@
+#include "unp.h"
+
+int main(int argc, const char *argv[])
+{
+    int listenfd, connfd, udpfd, nready, maxfd1;
+    char mesg[MAXLINE];
+    pid_t childpit;
+    fd_set rset;
+    ssize_t n;
+    socklen_t len;
+    const int on = 1;
+    struct sockaddr_in servaddr, cliaddr;
+    void sig_chld(int);
+
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    Setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+
+    Listen(listenfd, LISTENQ);
+
+    /**********/
+
+    udpfd = Socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+    Bind(udpfd, (SA *)&servaddr, sizeof(servaddr));
+    
+    /**********/
+
+    Signal(SIGCHLD, sig_chld);
+
+    FD_ZERO(&rset);
+    maxfd1 = max(listenfd, udpfd) + 1;
+    for (;;)
+    {
+        FD_SET(listenfd, &rset);
+        FD_SET(udpfd, &rset);
+        if ((nready = select(maxfd1, &rset, NULL, NULL, NULL)) < 0)
+        {
+            if (errno == EINTR)
+                continue;
+            else
+                err_sys("select error");
+        }
+
+        if (FD_ISSET(listenfd, &rset))
+        {
+            len = sizeof(cliaddr);
+            connfd = Accept(listenfd, (SA *)&cliaddr, &len);
+
+            if ((childpit = Fork()) == 0)
+            {
+                Close(listenfd);
+                my_str_echo(connfd);
+                exit(0);
+            }
+            Close(connfd);
+        }
+
+        if (FD_ISSET(udpfd, &rset))
+        {
+            len = sizeof(cliaddr);
+            n = Recvfrom(udpfd, mesg, MAXLINE, 0, (SA *)&cliaddr, &len);
+
+            Sendto(udpfd, mesg, n, 0, (SA *)&cliaddr, len);
+        }
+    }
+    return 0;
+}
+
